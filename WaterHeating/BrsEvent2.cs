1efbec824e158c0b4fa7623bb67353fdf11fd29f
Tomasz Raciborski (TRR) <TRR@kmd.dk>
2024-02-12 09:58:58 +0100

    brs event 2

---
+++ WaterHeating/BrsEvent2.cs
@@ -0,0 +1,17 @@
+﻿namespace WaterHeating;
+
+public class BrsEvent2
+{
+    public string BrsId { get; set; }
+    public string BrsName { get; set; }
+    public string Version { get; set; }
+    public string EventType { get; set; }
+
+    public string Payload1 { get; set; } // 
+    
+    public object Payload2 { get; set; } // 
+    
+    public Dictionary<string, string> Payload3 { get; set; } // 
+
+    // Payload.MeteringPointId
+}
